f2a4de2ce88a765e7ab02e1ca67bbedb91c4b45f
rakoo <matthieu.rakotojaona@gmail.com>
2012-12-08 00:10:54 +0100

    Make filldb-couchdb parse entire folders + validation
    There is now a check to see if the stations have lat/lon

--- filldb-couchdb.rb
+++ filldb-couchdb.rb
@@ -5,41 +5,53 @@ require 'trollop'
 
 abort("No couchdb url; quitting.") unless ARGV[0]
 COUCH = CouchRest.database ARGV[0]
-STATIONS = []
-SCHEDULES = []
 
-FILENAME = ARGV[1]
-abort("No data file; quitting.") unless FILENAME
+DIRNAME = ARGV[1]
+abort("No data dir; quitting.") unless DIRNAME
+abort("#{DIRNAME} is not a dir") unless File.directory?(DIRNAME)
 
 
-state = :stations
 
 # Fill the arrays ...
 COUCH.delete!
 COUCH.create!
-File.open(FILENAME).each do |line|
-  if line == "\n"
-    state = :schedule
-    next
+Dir.foreach(DIRNAME) do |entry|
+  file = File.absolute_path(entry, DIRNAME)
+  next unless (File.file?(file) and File.extname(file) == ".schedule")
+
+  stations = []
+  schedules = []
+
+  state = :stations
+  File.open(file).each do |line|
+    if line == "\n"
+      state = :schedule
+      next
+    end
+
+
+    if state == :stations
+      splitted = line.strip.split
+      lat, lon, name = if splitted[0].to_f != 0 && splitted[1].to_f != 0
+                         [splitted[0].to_f, splitted[1].to_f, splitted.drop(2).join(" ")]
+                       else
+                         [0, 0, splitted.join(" ")]
+                       end
+      stations << {:name => name, :lat => lat, :lon => lon}
+    elsif state == :schedule
+      schedules << line.split
+    end
+
   end
 
+  # ... and feed them to couch
 
-  if state == :stations
-    lat, lon, *name = line.strip.split
-    name = name.join " "
-    STATIONS << {:name => name, :lat => lat, :lon => lon}
-  elsif state == :schedule
-    SCHEDULES << line.split
-  end
+  schedule_doc = {
+    :type => :schedule,
+    :line => File.basename(file).split(".").first,
+    :stations => stations,
+    :schedules => schedules
+  }
 
+  COUCH.save_doc schedule_doc
 end
-
-# ... and feed them to couch
-
-schedule_doc = {
-  :type => :schedule,
-  :line => File.basename(FILENAME).split(".").first,
-  :stations => STATIONS,
-  :schedules => SCHEDULES
-}
-COUCH.save_doc schedule_doc
